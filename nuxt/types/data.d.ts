4d00dedc2304e1160eb12fbf935ab9a839576fe2
chika3742 <kazu.chika.shima@gmail.com>
2023-05-05 00:32:12 +0900

    add: データ用型定義

---
+++ nuxt/types/data.d.ts
@@ -0,0 +1,5 @@
+declare module "assets/data/release-notes.yaml" {
+  import {ReleaseNotes} from "~/types/generated/release-notes.g";
+  const releaseNotes: ReleaseNotes
+  export default releaseNotes
+}
