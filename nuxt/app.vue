282ae69617c1f96f7c81096475fb1266ca752ecd
chika3742 <kazu.chika.shima@gmail.com>
2023-05-06 18:36:47 +0900

    add: 読み込み中オーバーレイ

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -114,3 +114,37 @@ router.afterEach(() => {
 })
 
 </script>
+
+<style lang="sass" scoped>
+.loading-overlay
+  position: fixed
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+  z-index: 2000
+  backdrop-filter: blur(4px) brightness(0.5)
+  --webkit-backdrop-filter: blur(4px) brightness(0.5)
+
+  &::after
+    // loading indicator
+    content: ""
+    position: absolute
+    top: 50%
+    left: 50%
+    width: 100px
+    height: 100px
+    margin-top: -50px
+    margin-left: -50px
+    border-radius: 50%
+    border: 10px solid rgba(255, 255, 255, 0.2)
+    border-top-color: #fff
+    animation: spin 1s linear infinite
+
+    @keyframes spin
+      from
+        transform: rotate(0deg)
+      to
+        transform: rotate(360deg)
+
+</style>
