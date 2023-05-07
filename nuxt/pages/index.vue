cfc0a647e7d759a292ed5eab99f7ed0e482d40fd
chika3742 <kazu.chika.shima@gmail.com>
2023-05-07 21:27:38 +0900

    add: メインページに各ページへのリンク追加

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <h1>{{ $t("common.appName") }}</h1>
+    <h1 style="font-size: 1.5em">
+      {{ $t("common.appName") }}
+    </h1>
+    <v-list class="mt-4">
+      <v-list-item :title="$t('pageTitles.tpCalc')" to="/tp-calc" prepend-icon="mdi-sphere" />
+      <v-list-item :title="$t('pageTitles.warps')" to="/warps" prepend-icon="mdi-history" />
+    </v-list>
   </div>
 </template>
 
