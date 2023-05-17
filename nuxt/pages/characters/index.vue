8b5f9ac39064122ab69acbbda629529ed7fe50d1
chika3742 <kazu.chika.shima@gmail.com>
2023-05-17 22:49:37 +0900

    modify: キャラ絞り込みのレイアウト変更

--- nuxt/pages/characters/index.vue
+++ nuxt/pages/characters/index.vue
@@ -36,28 +36,36 @@ const filteredCharacters = computed(() => {
       <v-btn prepend-icon="mdi-filter">
         <span>{{ tx("common.filter") }}</span>
         <client-only>
-          <v-menu activator="parent" :close-on-content-click="false">
+          <v-menu activator="parent" :close-on-content-click="false" max-width="400px">
             <v-card>
-              <v-row no-gutters>
-                <div>
-                  <div class="filter-row-title">
-                    {{ tx("common.rarity") }}
-                  </div>
-                  <v-list v-model:selected="filter.rarity">
+              <div>
+                <div class="filter-row-title">
+                  {{ tx("common.rarity") }}
+                </div>
+                <v-list v-model:selected="filter.rarity">
+                  <v-row no-gutters>
                     <v-list-item v-for="rarity in [4, 5]" :key="rarity" :value="rarity">
                       <v-icon v-for="i of rarity" :key="i" :class="i !== 1 ? 'ml-n1' : ''" size="18" color="star">
                         mdi-star
                       </v-icon>
                     </v-list-item>
-                  </v-list>
+                  </v-row>
+                </v-list>
+              </div>
+              <v-divider />
+              <div>
+                <div class="filter-row-title">
+                  {{ tx("common.path") }}
                 </div>
-                <v-divider vertical />
-                <div>
-                  <div class="filter-row-title">
-                    {{ tx("common.path") }}
-                  </div>
-                  <v-list v-model:selected="filter.path">
-                    <v-list-item v-for="path in paths" :key="path" :title="tx(`paths.${path}` as const)" :value="path">
+                <v-list v-model:selected="filter.path">
+                  <v-row no-gutters>
+                    <v-list-item
+                      v-for="path in paths"
+                      :key="path"
+                      class="flex-grow-1"
+                      :title="tx(`paths.${path}` as const)"
+                      :value="path"
+                    >
                       <template #prepend>
                         <v-img
                           class="mr-2"
@@ -68,22 +76,30 @@ const filteredCharacters = computed(() => {
                         />
                       </template>
                     </v-list-item>
-                  </v-list>
+                  </v-row>
+                </v-list>
+              </div>
+              <v-divider />
+              <div>
+                <div class="filter-row-title">
+                  {{ tx("common.combatType") }}
                 </div>
-                <v-divider vertical />
-                <div>
-                  <div class="filter-row-title">
-                    {{ tx("common.combatType") }}
-                  </div>
-                  <v-list v-model:selected="filter.combatType">
-                    <v-list-item v-for="type in combatTypes" :key="type" :title="tx(`combatTypes.${type}` as const)" :value="type">
+                <v-list v-model:selected="filter.combatType">
+                  <v-row no-gutters>
+                    <v-list-item
+                      v-for="type in combatTypes"
+                      :key="type"
+                      class="flex-grow-1"
+                      :title="tx(`combatTypes.${type}` as const)"
+                      :value="type"
+                    >
                       <template #prepend>
                         <v-img class="mr-2" :src="getCombatTypeImage(type)" width="30" aspect-ratio="1" />
                       </template>
                     </v-list-item>
-                  </v-list>
-                </div>
-              </v-row>
+                  </v-row>
+                </v-list>
+              </div>
             </v-card>
           </v-menu>
         </client-only>
@@ -98,5 +114,6 @@ const filteredCharacters = computed(() => {
 <style scoped lang="sass">
 .filter-row-title
   padding: 4px 16px 0
-  text-align: center
+  margin-top: 8px
+  font-weight: bold
 </style>
