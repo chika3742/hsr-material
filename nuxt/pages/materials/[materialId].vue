1e3566add102b18f939eccea609a00a616ec2f81
chika3742 <kazu.chika.shima@gmail.com>
2023-05-21 00:15:52 +0900

    add: 仮の素材ページ

---
+++ nuxt/pages/materials/[materialId].vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+definePageMeta({
+  title: "materialDetails",
+  itemI18nKey: "materialNames",
+})
+</script>
+
+<template>
+  <div />
+</template>
+
+<style lang="sass" scoped>
+
+</style>
