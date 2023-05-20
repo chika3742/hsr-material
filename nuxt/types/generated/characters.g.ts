b67e33d6b882ccc359cc5bc38343acdf44e2af1e
chika3742 <kazu.chika.shima@gmail.com>
2023-05-20 21:57:11 +0900

    modify: 開拓者のバリアントに対応したデータ構造に変更

--- nuxt/types/generated/characters.g.ts
+++ nuxt/types/generated/characters.g.ts
@@ -1,20 +1,27 @@
 /* This file was generated. DO NOT edit by hand. */
 
-export type Path = "destruction" | "the_hunt" | "erudition" | "harmony" | "nihility" | "preservation" | "abundance";
-export type CombatType = "physical" | "fire" | "ice" | "lightning" | "wind" | "quantum" | "imaginary";
-export type Characters = Character[];
-
-export interface Character {
+export type Character = {
   id: string;
   $nameJA: string;
   rarity: number;
   path?: Path;
   combatType?: CombatType;
-  materials: CharacterMaterialDefinitions;
-}
+  materials?: CharacterMaterialDefinitions;
+  variants?: CharacterVariant[];
+} & Character1;
+export type Path = "destruction" | "the_hunt" | "erudition" | "harmony" | "nihility" | "preservation" | "abundance";
+export type CombatType = "physical" | "fire" | "ice" | "lightning" | "wind" | "quantum" | "imaginary";
+export type Character1 = {} | {};
+export type Characters = Character[];
+
 export interface CharacterMaterialDefinitions {
   common: string;
   ascension: string;
-  skills?: string;
+  skills: string;
   skillsAdvanced: string;
+}
+export interface CharacterVariant {
+  path: Path;
+  combatType: CombatType;
+  materials: CharacterMaterialDefinitions;
 }
