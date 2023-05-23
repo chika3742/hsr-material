77ab7a1ee722e53e3ec49df57fe89cc8cfdca5d7
chika3742 <kazu.chika.shima@gmail.com>
2023-05-23 10:58:25 +0900

    add: 素材一覧の作成

--- nuxt/pages/materials/index.vue
+++ nuxt/pages/materials/index.vue
@@ -1,11 +1,42 @@
 <script lang="ts" setup>
+import materials from "~/assets/data/materials.csv"
+import {splitByField} from "#imports"
+
 definePageMeta({
   title: "materials",
+})
+
+const groups = computed(() => {
+  return splitByField(materials, "category")
 })
 </script>
 
 <template>
-  <div />
+  <v-list style="user-select: none">
+    <v-list-group v-for="group in groups" :key="group[0].id">
+      <template #activator="{props}">
+        <v-list-item
+          :title="tx(`materialCategories.${group[0].category}`)"
+          v-bind="props"
+        >
+          <template #prepend>
+            <v-img :src="getMaterialImage(group[0].id)" aspect-ratio="1" class="mr-2" width="35px" />
+          </template>
+        </v-list-item>
+      </template>
+
+      <v-list-item
+        v-for="material in group"
+        :key="material.id"
+        :title="tx(`materialNames.${material.id}`)"
+        :to="localePath(`/materials/${material.id}`)"
+      >
+        <template #prepend>
+          <v-img :src="getMaterialImage(material.id)" aspect-ratio="1" class="mr-2" width="35px" />
+        </template>
+      </v-list-item>
+    </v-list-group>
+  </v-list>
 </template>
 
 <style lang="sass" scoped>
