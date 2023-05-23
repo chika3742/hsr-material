07e3bc8449ce315850574d80277ca9502e6dd2b5
chika3742 <kazu.chika.shima@gmail.com>
2023-05-23 11:13:19 +0900

    add: 素材一覧のレアリティによる絞り込み追加

--- nuxt/pages/materials/index.vue
+++ nuxt/pages/materials/index.vue
@@ -6,14 +6,37 @@ definePageMeta({
   title: "materials",
 })
 
+const filteringRarity = ref<number[]>([])
+
 const groups = computed(() => {
-  return splitByField(materials, "category")
+  const source = filteringRarity.value.length >= 1 ? materials.filter(e => e.rarity === filteringRarity.value[0]) : materials
+  return splitByField(source, "category")
 })
 </script>
 
 <template>
   <div>
-    <v-list style="user-select: none">
+    <v-btn :color="filteringRarity.length >= 1 ? 'star' : ''" prepend-icon="mdi-filter">
+      <span>{{ tx("common.filter") }}</span>
+
+      <v-menu activator="parent">
+        <v-list v-model:selected="filteringRarity">
+          <v-list-item
+            v-for="g in splitByField(materials, 'rarity')"
+            :key="g[0].rarity"
+            :value="g[0].rarity"
+          >
+            <v-row no-gutters>
+              <v-icon v-for="i in g[0].rarity" :key="i" color="star" size="18">
+                mdi-star
+              </v-icon>
+            </v-row>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-btn>
+
+    <v-list class="mt-4" style="user-select: none">
       <v-list-group v-for="group in groups" :key="group[0].id">
         <template #activator="{props}">
           <v-list-item
