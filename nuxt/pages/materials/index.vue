2f3d82e64c09eb01ad74bbd8dc895e7abe3d3a5e
chika3742 <kazu.chika.shima@gmail.com>
2023-05-23 11:05:53 +0900

    add: 素材一覧にレアリティ表示の追加

--- nuxt/pages/materials/index.vue
+++ nuxt/pages/materials/index.vue
@@ -12,31 +12,39 @@ const groups = computed(() => {
 </script>
 
 <template>
-  <v-list style="user-select: none">
-    <v-list-group v-for="group in groups" :key="group[0].id">
-      <template #activator="{props}">
+  <div>
+    <v-list style="user-select: none">
+      <v-list-group v-for="group in groups" :key="group[0].id">
+        <template #activator="{props}">
+          <v-list-item
+            :title="tx(`materialCategories.${group[0].category}`)"
+            v-bind="props"
+          >
+            <template #prepend>
+              <v-img :src="getMaterialImage(group[0].id)" aspect-ratio="1" class="mr-2" width="35px" />
+            </template>
+          </v-list-item>
+        </template>
+
         <v-list-item
-          :title="tx(`materialCategories.${group[0].category}`)"
-          v-bind="props"
+          v-for="material in group"
+          :key="material.id"
+          :to="localePath(`/materials/${material.id}`)"
         >
           <template #prepend>
-            <v-img :src="getMaterialImage(group[0].id)" aspect-ratio="1" class="mr-2" width="35px" />
+            <v-img :src="getMaterialImage(material.id)" aspect-ratio="1" class="mr-2" width="35px" />
           </template>
+          <v-row align="center" no-gutters style="gap: 8px">
+            <v-list-item-title>{{ tx(`materialNames.${material.id}`) }}</v-list-item-title>
+            <v-chip :color="`rank${material.rarity}`" size="small">
+              <v-icon>mdi-star</v-icon>
+              {{ material.rarity }}
+            </v-chip>
+          </v-row>
         </v-list-item>
-      </template>
-
-      <v-list-item
-        v-for="material in group"
-        :key="material.id"
-        :title="tx(`materialNames.${material.id}`)"
-        :to="localePath(`/materials/${material.id}`)"
-      >
-        <template #prepend>
-          <v-img :src="getMaterialImage(material.id)" aspect-ratio="1" class="mr-2" width="35px" />
-        </template>
-      </v-list-item>
-    </v-list-group>
-  </v-list>
+      </v-list-group>
+    </v-list>
+  </div>
 </template>
 
 <style lang="sass" scoped>
