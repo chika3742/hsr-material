636b1c2f4bcf2d5c0741337994d0187603392c65
chika3742 <kazu.chika.shima@gmail.com>
2023-05-25 23:22:28 +0900

    add: 光円錐リストページ

---
+++ nuxt/pages/light-cones/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import GroupedList from "~/components/grouped-list.vue"
+import lightCones from "~/assets/data/light-cones.yaml"
+
+definePageMeta({
+  title: "lightCones",
+})
+
+const items = computed(() => {
+  return lightCones.sort((a, b) => b.rarity - a.rarity)
+})
+</script>
+
+<template>
+  <GroupedList
+    :image-func="getLightConeImage"
+    :items="items"
+    category-field="path"
+    category-i18n-key="paths"
+    item-i18n-key="lightConeNames"
+    link-base-path="/light-cones"
+  />
+</template>
+
+<style lang="sass" scoped />
