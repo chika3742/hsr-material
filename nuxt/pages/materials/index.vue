664bf1cd6fa4262e50dd398519075d7075fc4bf2
chika3742 <kazu.chika.shima@gmail.com>
2023-05-30 19:29:15 +0900

    fix: node mismatchエラー

--- nuxt/pages/materials/index.vue
+++ nuxt/pages/materials/index.vue
@@ -19,21 +19,23 @@ const materialList = computed(() => {
     <v-btn :color="filteringRarity.length >= 1 ? 'star' : ''" prepend-icon="mdi-filter">
       <span>{{ tx("common.filter") }}</span>
 
-      <v-menu activator="parent">
-        <v-list v-model:selected="filteringRarity">
-          <v-list-item
-            v-for="g in splitByField(materials, 'rarity')"
-            :key="g[0].rarity"
-            :value="g[0].rarity"
-          >
-            <v-row no-gutters>
-              <v-icon v-for="i in g[0].rarity" :key="i" color="star" size="18">
-                mdi-star
-              </v-icon>
-            </v-row>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      <client-only>
+        <v-menu activator="parent">
+          <v-list v-model:selected="filteringRarity">
+            <v-list-item
+              v-for="g in splitByField(materials, 'rarity')"
+              :key="g[0].rarity"
+              :value="g[0].rarity"
+            >
+              <v-row no-gutters>
+                <v-icon v-for="i in g[0].rarity" :key="i" color="star" size="18">
+                  mdi-star
+                </v-icon>
+              </v-row>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </client-only>
     </v-btn>
 
     <GroupedList
