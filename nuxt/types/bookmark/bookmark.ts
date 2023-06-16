7bbaf946bc58c98e79cedb7d3c177a0f8cfd919a
chika3742 <kazu.chika.shima@gmail.com>
2023-06-16 21:39:34 +0900

    add: ブックマーク保存用Interface

---
+++ nuxt/types/bookmark/bookmark.ts
@@ -0,0 +1,63 @@
+import {Stat} from "~/types/generated/relic-stats.g"
+import {Usage} from "~/types/bookmark/usage"
+
+export type Bookmark =
+  | Bookmark.CharacterMaterial
+  | Bookmark.LightConeMaterial
+  | Bookmark.Exp
+  | Bookmark.RelicSet
+  | Bookmark.RelicPiece
+
+export type LevelingBookmark = Bookmark.CharacterMaterial | Bookmark.LightConeMaterial | Bookmark.Exp
+
+export namespace Bookmark {
+  export interface CharacterMaterial {
+    id?: number
+    type: "character_material"
+    bookmarkedAt: Date
+    materialId: string
+    usage: Usage.Character
+    quantity: number
+  }
+
+  export interface LightConeMaterial {
+    id?: number
+    type: "light_cone_material"
+    bookmarkedAt: Date
+    materialId: string
+    usage: Usage.LightCone
+    quantity: number
+  }
+
+  export interface Exp {
+    id?: number
+    type: "exp"
+    bookmarkedAt: Date
+    usage: Usage.Exp
+    exp: number
+    selectedItem: string
+  }
+
+  export interface RelicSet {
+    id?: number
+    type: "relic_set"
+    bookmarkedAt: Date
+    characterId: string
+    relicSetIds: string[]
+    mainStats: {
+      body: Stat[]
+      feet: Stat[]
+    }
+    subStats: Stat[]
+  }
+
+  export interface RelicPiece {
+    id?: number
+    type: "relic_piece"
+    bookmarkedAt: Date
+    relicPieceId: string
+    characterId: string
+    mainStat: Stat | null
+    subStats: Stat[]
+  }
+}
