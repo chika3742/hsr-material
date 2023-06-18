d3e14c53e805bf2987daf503d38751506fe653ab
chika3742 <kazu.chika.shima@gmail.com>
2023-06-18 18:37:54 +0900

    [#13] fix: purposeTypesの型

--- nuxt/components/single-slider-panel.vue
+++ nuxt/components/single-slider-panel.vue
@@ -5,7 +5,7 @@
       <MaterialCards
         :items="items"
         :range="range"
-        purpose-types="ascension"
+        :purpose-types="['ascension']"
         class="mt-2"
       />
     </v-expansion-panel-text>
