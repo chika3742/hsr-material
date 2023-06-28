25de57c9f3061a338b7e5d185175c1d4d9476321
chika3742 <kazu.chika.shima@gmail.com>
2023-06-28 18:59:52 +0900

    update: 跳躍天井カウンターWindows用コマンドの更新とコピー機能の削除

--- nuxt/components/warps/description.vue
+++ nuxt/components/warps/description.vue
@@ -1,19 +1,10 @@
 <script setup lang="ts">
 const {$marked} = useNuxtApp()
-const snackbar = useSnackbar()
 const i18n = useI18n()
 
 const marked = $marked({gfm: false})
 
 const streamUrl = "https://apps.apple.com/app/stream/id1312141691"
-
-const windowsCommandCode = ref<HTMLElement>()
-
-const copyWindowsCommand = () => {
-  window.navigator.clipboard.writeText(windowsCommandCode.value!.innerText)
-  snackbar.show("コピーしました")
-}
-
 </script>
 
 <template>
@@ -29,16 +20,6 @@ const copyWindowsCommand = () => {
       <v-expansion-panel-title>{{ $t("warpsPage.howToGetUrl.windows.title") }}</v-expansion-panel-title>
       <v-expansion-panel-text>
         <div v-html="marked.parse(i18n.t('warpsPage.howToGetUrl.windows.contents'))" />
-        <client-only>
-          <teleport to="#windows-script">
-            <div class="d-flex flex-nowrap align-center">
-              <pre>
-                <code ref="windowsCommandCode">iex "&{$(irm https://gist.githubusercontent.com/chika3742/0542db8a5e329fb15af9e37714e92893/raw/899a52ae891cc150654d8428df8d57b141746446/getlink.ps1)} global"</code>
-              </pre>
-              <v-btn class="ml-2" flat icon="ms:content_copy" @click="copyWindowsCommand" />
-            </div>
-          </teleport>
-        </client-only>
       </v-expansion-panel-text>
     </v-expansion-panel>
 
