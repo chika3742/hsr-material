dd19b2dd8cfdba520256a82e9cb8ed6d1dd93138
chika3742 <kazu.chika.shima@gmail.com>
2023-07-02 18:52:55 +0900

    remove: getWarpHistory 関数の最小インスタンス数設定削除

--- firebase/functions/src/funcs/get-warp-history.ts
+++ firebase/functions/src/funcs/get-warp-history.ts
@@ -4,9 +4,6 @@ import {GetWarpHistoryParams} from "../types/get-warp-history-params"
 
 export const getWarpHistory = functions
   .region("asia-northeast1")
-  .runWith({
-    minInstances: 1,
-  })
   .tasks.taskQueue({
     rateLimits: {
       maxConcurrentDispatches: 5,
