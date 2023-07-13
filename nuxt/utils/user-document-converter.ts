74c398287930fe873939a4f00fe32e54ca528322
chika3742 <kazu.chika.shima@gmail.com>
2023-07-13 22:55:35 +0900

    fix: userDocumentConverterをFunctionからObjectに

--- nuxt/utils/user-document-converter.ts
+++ nuxt/utils/user-document-converter.ts
@@ -4,24 +4,22 @@ import {UserDocument} from "~/types/firestore/user-document"
 /**
  * Creates a FirestoreDataConverter that does not convert anything. This only
  */
-export const userDocumentConverter = (): FirestoreDataConverter<UserDocument> => {
-  return {
-    toFirestore: (modelObject: UserDocument) => modelObject,
-    fromFirestore(snapshot: QueryDocumentSnapshot<UserDocument>): UserDocument {
-      const data = snapshot.data()
+export const userDocumentConverter: FirestoreDataConverter<UserDocument> = {
+  toFirestore: (modelObject: UserDocument) => modelObject,
+  fromFirestore(snapshot: QueryDocumentSnapshot<UserDocument>): UserDocument {
+    const data = snapshot.data()
 
-      return {
-        ...data,
-        data: {
-          warps: data.data.warps,
-          bookmarks: data.data.bookmarks.map(bookmark => ({
-            ...bookmark,
-            // convert Timestamp to Date
-            // @ts-ignore
-            bookmarkedAt: new Timestamp(bookmark.bookmarkedAt.seconds, bookmark.bookmarkedAt.nanoseconds).toDate(),
-          })),
-        },
-      }
-    },
-  }
+    return {
+      ...data,
+      data: {
+        warps: data.data.warps,
+        bookmarks: data.data.bookmarks.map(bookmark => ({
+          ...bookmark,
+          // convert Timestamp to Date
+          // @ts-ignore
+          bookmarkedAt: new Timestamp(bookmark.bookmarkedAt.seconds, bookmark.bookmarkedAt.nanoseconds).toDate(),
+        })),
+      },
+    }
+  },
 }
