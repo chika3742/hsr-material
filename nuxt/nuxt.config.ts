f4dd7f2d47ab0e5c9ba64b64381726056864fd4e
chika3742 <kazu.chika.shima@gmail.com>
2023-08-01 21:50:04 +0900

    fix: precache設定の変更

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -152,7 +152,6 @@ export default defineNuxtConfig({
   },
 
   googleFonts: {
-    download: false,
     families: {
       "M PLUS 2": [500, 700],
       "Kaisei Opti": [700],
@@ -174,28 +173,15 @@ export default defineNuxtConfig({
       navigateFallbackAllowlist: [/^\/$/],
     },
     workbox: {
-      globDirectory: "dist",
-      globPatterns: ["_nuxt/*.{js,css,woff2}"],
+      globPatterns: [
+        "**/*.{js,css}",
+      ],
       runtimeCaching: [
         {
-          urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+          urlPattern: /.*\.woff2$/i,
           handler: "CacheFirst",
           options: {
             cacheName: "google-fonts-cache",
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-        {
-          urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-          handler: "CacheFirst",
-          options: {
-            cacheName: "gstatic-fonts-cache",
             expiration: {
               maxEntries: 10,
               maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
