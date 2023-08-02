1a8e09e0a3fbce11f56cbb3843fb163068251194
chika3742 <kazu.chika.shima@gmail.com>
2023-08-02 23:32:28 +0900

    clean: client-onlyの囲いを削除
    node mismatchエラーが解決された模様

--- nuxt/components/app/footer.vue
+++ nuxt/components/app/footer.vue
@@ -34,20 +34,17 @@
         </v-btn>
         <v-btn prepend-icon="mdi-earth" variant="text" density="comfortable">
           LANG
-          <!-- to avoid hydration node mismatch error -->
-          <client-only>
-            <v-menu activator="parent">
-              <v-list>
-                <v-list-item
-                  v-for="lang in availableLocales"
-                  :key="lang.code"
-                  :to="switchLocalePath(lang.code)"
-                >
-                  <v-list-item-title>{{ lang.name }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </client-only>
+          <v-menu activator="parent">
+            <v-list>
+              <v-list-item
+                v-for="lang in availableLocales"
+                :key="lang.code"
+                :to="switchLocalePath(lang.code)"
+              >
+                <v-list-item-title>{{ lang.name }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
         </v-btn>
         <v-btn prepend-icon="mdi-brightness-4" variant="text" density="comfortable">
           {{ $t('footer.theme') }}
