aae1e494510e6667053ec6f46b7f05aa9b6f1514
chika3742 <kazu.chika.shima@gmail.com>
2023-08-30 21:15:08 +0900

    fix: volarでlocalePath, switchLocalePathが解決できない問題の修正

--- nuxt/index.d.ts
+++ nuxt/index.d.ts
@@ -1,3 +1,6 @@
+import {RawLocation, RouteLocation} from "@intlify/vue-router-bridge";
+import {Locale} from "@intlify/vue-i18n-bridge";
+
 declare module "nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     title: string
@@ -8,6 +11,9 @@ declare module "nuxt/dist/pages/runtime/composables" {
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     readonly $isTouchDevice: boolean
+    // shim for volar
+    localePath: (route: RawLocation | RouteLocation, locale?: Locale) => string;
+    switchLocalePath: (locale?: Locale) => string;
   }
 }
 
