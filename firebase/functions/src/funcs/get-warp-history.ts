a4710e716b747c4a6260f3db6c44c9d78e1d647e
Chikamatsu Kazuya <43089218+chika3742@users.noreply.github.com>
2023-09-15 22:18:13 +0900

    feat(firebase): Firebase Functions Gen2に移行 (#183)

--- firebase/functions/src/funcs/get-warp-history.ts
+++ firebase/functions/src/funcs/get-warp-history.ts
@@ -1,13 +1,12 @@
-import functions from "firebase-functions"
+import {onTaskDispatched} from "firebase-functions/v2/tasks"
 import {WarpHistoryHandler} from "../lib/warp-history-handler.js"
 import {GetWarpHistoryParams} from "../types/get-warp-history-params"
 
-export const getWarpHistory = functions
-  .region("asia-northeast1")
-  .tasks.taskQueue({
-    rateLimits: {
-      maxConcurrentDispatches: 5,
-    },
-  }).onDispatch(async(data: GetWarpHistoryParams) => {
-    await new WarpHistoryHandler().get(data)
-  })
+export const getWarpHistory = onTaskDispatched<GetWarpHistoryParams>({
+  region: "asia-northeast1",
+  rateLimits: {
+    maxConcurrentDispatches: 5,
+  },
+}, async({data}) => {
+  await new WarpHistoryHandler().get(data)
+})
