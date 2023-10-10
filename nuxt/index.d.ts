e5c50c92cb06975947d612afc80e9ec0ac2cee89
chika3742 <kazu.chika.shima@gmail.com>
2023-10-10 16:58:13 +0900

    fix: Firebaseプラグインのprovideのタイピング

--- nuxt/index.d.ts
+++ nuxt/index.d.ts
@@ -1,5 +1,9 @@
-import {RawLocation, RouteLocation} from "@intlify/vue-router-bridge";
-import {Locale} from "@intlify/vue-i18n-bridge";
+import {RawLocation, RouteLocation} from "@intlify/vue-router-bridge"
+import {Locale} from "@intlify/vue-i18n-bridge"
+import {Auth} from "@firebase/auth"
+import {Firestore} from "@firebase/firestore"
+import {Functions} from "@firebase/functions"
+import {Analytics} from "@firebase/analytics"
 
 declare module "nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
@@ -8,12 +12,21 @@ declare module "nuxt/dist/pages/runtime/composables" {
   }
 }
 
+declare module "#app" {
+  interface NuxtApp {
+    $auth: Auth
+    $firestore: Firestore
+    $functions: Functions
+    $analytics: Analytics
+  }
+}
+
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     readonly $isTouchDevice: boolean
     // shim for volar
-    localePath: (route: RawLocation | RouteLocation, locale?: Locale) => string;
-    switchLocalePath: (locale?: Locale) => string;
+    localePath: (route: RawLocation | RouteLocation, locale?: Locale) => string
+    switchLocalePath: (locale?: Locale) => string
   }
 }
 
