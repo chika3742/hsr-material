2048e706cbf752d2e2a51f6faa7b804674ba7264
chika3742 <kazu.chika.shima@gmail.com>
2023-12-28 22:42:24 +0900

    add: ルアン・メェイ、雪衣、鏡の中の私

---
+++ nuxt/assets/img/characters/convert.sh
@@ -0,0 +1,9 @@
+for file in *_small.png; do
+    convert "$file" -crop 175x175+0+0 "$file"
+done
+
+for file in *.png; do
+    echo "$file"
+    cwebp -q 80 "$file" -o "${file%.png}.webp" > /dev/null
+    rm "$file"
+done
