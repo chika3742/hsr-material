4c9e72173724ba266af92cdae9ddb683a293cd4c
chika3742 <kazu.chika.shima@gmail.com>
2024-06-21 21:20:25 +0900

    feat: OGP対応

--- packages/nuxt/app.vue
+++ packages/nuxt/app.vue
@@ -221,6 +221,15 @@ useHead({
   titleTemplate: `%s - ${i18n.t("common.appName")}`,
 })
 
+useSeoMeta({
+  ogImage: "https://hsr.matnote.app/favicon.webp",
+  ogTitle: "崩壊：スターレイル 素材ノート",
+  ogSiteName: "崩壊：スターレイル 素材ノート",
+  ogType: "website",
+  twitterCard: "summary",
+  twitterSite: "@chikavoid",
+})
+
 let unsubscribeAuthListener: Unsubscribe | null = null
 
 onBeforeMount(() => {
