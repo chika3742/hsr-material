f540f713985f98c122fa205390036560a67c941a
chika3742 <kazu.chika.shima@gmail.com>
2025-02-06 10:09:09 +0900

    Revert "ソースマップに対応、跳躍履歴取得時にエラーが発生する問題の修正"
    This reverts commit 556e9496e9fc26cd04e158f9eccd1a41f742b87f.

--- packages/firebase/functions/src/index.ts
+++ packages/firebase/functions/src/index.ts
@@ -1,11 +1,6 @@
 import admin from "firebase-admin"
-import "source-map-support/register"
-import { initializeFirestore } from "firebase-admin/firestore";
 
-const app = admin.initializeApp()
-initializeFirestore(app, {
-  preferRest: true,
-})
+admin.initializeApp()
 
 export * from "./funcs/dispatch-get-warp-history.js"
 export * from "./funcs/get-warp-history.js"
