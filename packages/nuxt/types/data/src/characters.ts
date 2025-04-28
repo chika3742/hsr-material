0d105bd896e4e0de6e8a68cc26dad9b9dc5ed810
chika3742 <kazu.chika.shima@gmail.com>
2025-04-28 18:33:13 +0900

    Refactor character data structure

--- packages/nuxt/types/data/src/characters.ts
+++ packages/nuxt/types/data/src/characters.ts
@@ -1,22 +1,34 @@
 import type { HsrCombatType, HsrPath } from "../enums"
-import type { MaterialExpr } from "../ingredients"
+import type { MaterialExpr } from "../ingredient"
 
-interface CharacterBase<T> {
+interface CharacterBase {
   id: string
   $nameJA: string
   yomi: string
   rarity: number
-  materials?: Record<string, MaterialExpr>
-  levelingItemTable?: string
-  variants?: (T & {
+}
+
+interface CharacterWithVariantsBase<T> extends CharacterBase {
+  variants: (T & {
     materials: Record<string, MaterialExpr>
     levelingItemTable?: string
   })[]
 }
 
-export interface HsrCharacter extends CharacterBase<HsrCharacter> {
-  path?: HsrPath
-  combatType?: HsrCombatType
+interface CharacterWithoutVariantsBase extends CharacterBase {
+  materials: Record<string, MaterialExpr>
+  levelingItemTable?: string
 }
+
+interface HsrCharacterMixin {
+  path: HsrPath
+  combatType: HsrCombatType
+}
+
+/** Character for HSR w/ variants */
+export type HsrCharacterWV = CharacterWithVariantsBase<HsrCharacterMixin>
+/** Character for HSR w/o variants */
+export type HsrCharacterWoV = (CharacterWithoutVariantsBase & HsrCharacterMixin)
+export type HsrCharacter = HsrCharacterWV | HsrCharacterWoV
 
 export type Characters = HsrCharacter[]
