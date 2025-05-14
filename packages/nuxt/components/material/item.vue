bbf91108e49fb6f193863fe1e0a68668e3e21e64
chika3742 <kazu.chika.shima@gmail.com>
2025-05-14 16:45:53 +0900

    Refactor MaterialCard component to reduce the scope of responsibility
    Moved the process to handle exp items to nuxt package.

--- packages/nuxt/components/material/item.vue
+++ packages/nuxt/components/material/item.vue
@@ -11,7 +11,8 @@ import { db } from "~/libs/db/providers"
 import characterIngredients from "~/assets/data/character-ingredients.yaml"
 import lightConeIngredients from "~/assets/data/light-cone-ingredients.yaml"
 import type { BookmarkableExp, BookmarkableIngredient, BookmarkableMaterial } from "~/types/bookmark/bookmarkables"
-import { isBookmarkableExp, isBookmarkableMaterial } from "~/types/bookmark/bookmarkables"
+import { isBookmarkableExp } from "~/types/bookmark/bookmarkables"
+import type { Material } from "~/types/data/src/materials"
 
 interface Props {
   /**
@@ -37,30 +38,49 @@ const snackbar = useSnackbar()
 const i18n = useI18n()
 
 const loading = ref(false)
+const currentExpItemIndex = ref(0)
 
-const getRarity = (materialId: string) => {
-  const material = materials.find(e => e.id === materialId)!
-  return material.rarity
+const expItems = computed(() => {
+  if (props.items[0].type === "character_exp") {
+    return characterIngredients.expItems
+  } else if (props.items[0].type === "light_cone_exp") {
+    return lightConeIngredients.expItems
+  } else {
+    return null
+  }
+})
+
+const currentExpItem = computed(() => {
+  return expItems.value?.[currentExpItemIndex.value] ?? null
+})
+
+const forwardExpItem = () => {
+  currentExpItemIndex.value = (currentExpItemIndex.value + 1) % expItems.value!.length
 }
+
+const material = computed(() => {
+  const firstItem = props.items[0]
+  let result: Material | undefined
+  if (isBookmarkableExp(firstItem)) {
+    result = materials.find(e => e.id === currentExpItem.value!.itemId)
+  } else {
+    result = materials.find(e => e.id === firstItem.materialId)
+  }
+  if (typeof result === "undefined") {
+    throw new Error("Material not found")
+  }
+
+  return result
+})
 
 const quantity = computed(() => {
   if (isBookmarkableExp(props.items[0])) {
     const items = props.items as BookmarkableExp[]
-    return items.reduce((acc, e) => acc + e.exp, 0)
+    const exp = items.reduce((acc, e) => acc + e.exp, 0)
+    return Math.ceil(exp / currentExpItem.value!.expPerItem)
   } else {
     const items = props.items as BookmarkableMaterial[]
     return items.reduce((acc, e) => acc + e.quantity, 0)
-  }
-})
-
-const expItemLineup = computed(() => {
-  switch (props.items[0].type) {
-    case "character_exp":
-      return characterIngredients.expItems
-    case "light_cone_exp":
-      return lightConeIngredients.expItems
-    default:
-      return []
   }
 })
 
@@ -116,7 +136,7 @@ const bookmarkState = computed<"full" | "partial" | "none" | undefined>(() => {
   }
 })
 
-const toggleBookmark = async (selectedExpItemId: string | undefined) => {
+const toggleBookmark = async () => {
   if (savedBookmarks.value === null) {
     return
   }
@@ -125,7 +145,7 @@ const toggleBookmark = async (selectedExpItemId: string | undefined) => {
 
   try {
     if (bookmarkState.value === "none") {
-      await db.bookmarks.addLevelingItems(props.items.map(e => JSON.parse(JSON.stringify(e)) as BookmarkableIngredient), selectedExpItemId, props.itemId)
+      await db.bookmarks.addLevelingItems(props.items.map(e => JSON.parse(JSON.stringify(e)) as BookmarkableIngredient), material.value.id, props.itemId)
       snackbar.show(tx(i18n, "bookmark.bookmarked"))
     } else {
       const result = await db.bookmarks.remove(...savedBookmarks.value.map(e => e.id!))
@@ -144,12 +164,12 @@ const toggleBookmark = async (selectedExpItemId: string | undefined) => {
   }
 }
 
-const reBookmark = async (selectedExpItemId: string | undefined) => {
+const reBookmark = async () => {
   loading.value = true
 
   try {
     await db.bookmarks.remove(...(savedBookmarks.value as LevelingBookmark[]).map(e => e.id!))
-    await db.bookmarks.addLevelingItems(props.items, selectedExpItemId)
+    await db.bookmarks.addLevelingItems(props.items, currentExpItem.value!.itemId)
 
     snackbar.show(tx(i18n, "bookmark.bookmarked"))
   } catch (e) {
@@ -163,18 +183,18 @@ const reBookmark = async (selectedExpItemId: string | undefined) => {
 
 <template>
   <MaterialCard
-    :bookmark-button-loading="loading"
+    :name="localize(material.name)"
+    :to="`/materials/${material.id}`"
+    :loading="loading || bookmarkState === undefined"
     :bookmark-state="bookmarkState"
     :dimmed="typeof itemId !== 'undefined' && bookmarkState === 'none'"
-    :exp-item-lineup="expItemLineup"
-    :initial-selected-exp-item-id="initialSelectedExpItem"
-    :is-exp-item="isBookmarkableExp(items[0])"
-    :material-id="isBookmarkableMaterial(items[0]) ? items[0].materialId : undefined"
-    :material-image="getMaterialImage"
+    :show-item-toggle-button="isBookmarkableExp(items[0])"
+    :image-path="getMaterialImage(material.id)"
     :quantity="quantity"
-    :rarity="getRarity"
-    :farming-count="showFarmingCount ? getFarmingCount : undefined"
+    :rarity="material.rarity"
+    :farming-count="showFarmingCount ? getFarmingCount(material.id, quantity) : null"
     @toggle-bookmark="toggleBookmark"
     @re-bookmark="reBookmark"
+    @toggle-item="forwardExpItem"
   />
 </template>
