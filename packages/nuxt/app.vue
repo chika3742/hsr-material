b160d47d4408d155f32c13455311c520dc56c218
chika3742 <kazu.chika.shima@gmail.com>
2025-05-23 10:48:54 +0900

    Fix: does not change drawer title depending on language setting

--- packages/nuxt/app.vue
+++ packages/nuxt/app.vue
@@ -155,79 +155,81 @@ const isProd = rConfig.public.isProdBranch
 const repositoryUrl = "https://github.com/chika3742/hsr-material"
 const feedbackPageUrl = "https://www.chikach.net/hsr-material-fb"
 const hoyolabArticleUrl = "https://www.hoyolab.com/article/18406761"
-const drawerItems: DrawerItemOrDivider[] = [
-  {
-    title: tx("pageTitles.home"),
-    icon: "mdi-home",
-    to: "/",
-  },
-  {
-    title: tx("pageTitles.bookmarks"),
-    icon: "mdi-bookmark-multiple",
-    to: "/bookmarks",
-  },
-  {
-    icon: "mdi-import",
-    title: tx("navDrawer.gameDataSync"),
-    onClick() {
-      showGameDataSyncDialog.value = true
+const drawerItems = computed<DrawerItemOrDivider[]>(() => {
+  return [
+    {
+      title: tx(i18n, "pageTitles.home"),
+      icon: "mdi-home",
+      to: "/",
     },
-  },
-  "---" as const,
-  {
-    title: tx("pageTitles.characters"),
-    icon: "mdi-account",
-    to: "/characters",
-  },
-  {
-    title: tx("pageTitles.lightCones"),
-    icon: "mdi-cone",
-    to: "/light-cones",
-  },
-  {
-    title: tx("pageTitles.relics"),
-    icon: "mdi-star-david",
-    to: "/relics",
-  },
-  {
-    title: tx("pageTitles.materials"),
-    icon: "mdi-grass",
-    to: "/materials",
-  },
-  "---" as const,
-  {
-    title: tx("pageTitles.warps"),
-    icon: "ms:history",
-    to: "/warps",
-  },
-  {
-    title: tx("pageTitles.tpCalc"),
-    icon: "mdi-sphere",
-    to: "/tp-calc",
-  },
-  {
-    title: tx("pageTitles.sync"),
-    icon: "mdi-book-sync",
-    to: "/sync",
-  },
-  "---" as const,
-  {
-    title: tx("pageTitles.settings"),
-    icon: "mdi-cog",
-    to: "/settings",
-  },
-  {
-    title: tx("pageTitles.about"),
-    icon: "mdi-information",
-    to: "/about",
-  },
-  {
-    title: tx("navDrawer.kofi"),
-    icon: "mdi-coffee",
-    href: "https://ko-fi.com/chika3742",
-    target: "_blank",
-  },
-]
+    {
+      title: tx(i18n, "pageTitles.bookmarks"),
+      icon: "mdi-bookmark-multiple",
+      to: "/bookmarks",
+    },
+    {
+      icon: "mdi-import",
+      title: tx(i18n, "navDrawer.gameDataSync"),
+      onClick() {
+        showGameDataSyncDialog.value = true
+      },
+    },
+    "---" as const,
+    {
+      title: tx(i18n, "pageTitles.characters"),
+      icon: "mdi-account",
+      to: "/characters",
+    },
+    {
+      title: tx(i18n, "pageTitles.lightCones"),
+      icon: "mdi-cone",
+      to: "/light-cones",
+    },
+    {
+      title: tx(i18n, "pageTitles.relics"),
+      icon: "mdi-star-david",
+      to: "/relics",
+    },
+    {
+      title: tx(i18n, "pageTitles.materials"),
+      icon: "mdi-grass",
+      to: "/materials",
+    },
+    "---" as const,
+    {
+      title: tx(i18n, "pageTitles.warps"),
+      icon: "ms:history",
+      to: "/warps",
+    },
+    {
+      title: tx(i18n, "pageTitles.tpCalc"),
+      icon: "mdi-sphere",
+      to: "/tp-calc",
+    },
+    {
+      title: tx(i18n, "pageTitles.sync"),
+      icon: "mdi-book-sync",
+      to: "/sync",
+    },
+    "---" as const,
+    {
+      title: tx(i18n, "pageTitles.settings"),
+      icon: "mdi-cog",
+      to: "/settings",
+    },
+    {
+      title: tx(i18n, "pageTitles.about"),
+      icon: "mdi-information",
+      to: "/about",
+    },
+    {
+      title: tx(i18n, "navDrawer.kofi"),
+      icon: "mdi-coffee",
+      href: "https://ko-fi.com/chika3742",
+      target: "_blank",
+    },
+  ]
+})
 
 const isDrawerOpenOnMobile = ref(false)
 const mounted = ref(false)
