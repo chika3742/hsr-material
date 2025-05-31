129316042dec222563f7264d0714c3f63173b95f
Chikamatsu Kazuya <43089218+chika3742@users.noreply.github.com>
2025-05-31 18:13:34 +0900

    Ascension materials are now treated as the same bookmark in the variants (#507)
    * Ascension materials are now treated as the same bookmark in the variants
    
    * Add migration logic
    
    * Refactor: move migration logic to ~/dexie/migrate.ts
    
    * Add migration test

--- packages/nuxt/dexie/migrate.ts
+++ packages/nuxt/dexie/migrate.ts
@@ -1,4 +1,4 @@
-import _ from "lodash"
+import { omit, uniqBy } from "lodash-es"
 import hash from "object-hash"
 import type { SyncedUserData } from "~/types/firestore/user-document"
 
@@ -28,7 +28,7 @@ export const migrate = (data: { [table: string]: any }, oldVersion: number, newV
       if (!["character_material", "light_cone_material", "character_exp", "light_cone_exp"].includes(bookmark.type)) {
         continue
       }
-      bookmark.hash = hash(_.omit(bookmark, ["id", "bookmarkedAt", "selectedItem"]))
+      bookmark.hash = hash(omit(bookmark, ["id", "bookmarkedAt", "selectedItem"]))
     }
 
     oldVersion++
@@ -40,8 +40,33 @@ export const migrate = (data: { [table: string]: any }, oldVersion: number, newV
         continue
       }
       bookmark.characterId = "march-7th_preservation"
-      bookmark.hash = hash(_.omit(bookmark, ["id", "bookmarkedAt", "selectedItem", "hash"]))
+      bookmark.hash = hash(omit(bookmark, ["id", "bookmarkedAt", "selectedItem", "hash"]))
     }
+    oldVersion++
+  }
+
+  if (oldVersion === 4) {
+    // Migrate ascension bookmarks for Trailblazer and March 7th
+    const characterIdMap: Record<string, string> = {
+      "trailblazer_destruction": "trailblazer",
+      "trailblazer_preservation": "trailblazer",
+      "trailblazer_harmony": "trailblazer",
+      "trailblazer_remembrance": "trailblazer",
+      "march-7th_preservation": "march-7th",
+      "march-7th_the_hunt": "march-7th",
+    }
+
+    for (const bookmark of data.bookmarks) {
+      if (["character_material", "character_exp"].includes(bookmark.type)
+        && bookmark.usage.purposeType === "ascension"
+        && Object.keys(characterIdMap).includes(bookmark.characterId)) {
+        bookmark.characterId = characterIdMap[bookmark.characterId]
+        bookmark.hash = hash(omit(bookmark, ["id", "bookmarkedAt", "selectedItem", "hash"]))
+      }
+    }
+    // remove duplicates
+    data.bookmarks = uniqBy<any>(data.bookmarks, e => e.hash)
+    oldVersion++
   }
 
   return data as SyncedUserData
