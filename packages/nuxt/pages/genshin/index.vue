c1df57447a805fd4a2eadaa4a752489f7a417233
chika3742 <kazu.chika.shima@gmail.com>
2025-06-01 22:48:13 +0900

    Create index.vue

--- packages/nuxt/pages/genshin/index.vue
+++ packages/nuxt/pages/genshin/index.vue
@@ -1,3 +1,77 @@
 <template>
-  <div />
+  <div class="d-flex flex-column ga-4">
+    <h1 style="font-size: 1.5em">
+      {{ $t("indexPageGenshin.title") }}
+    </h1>
+    <p>{{ tx("indexPageGenshin.desc") }}</p>
+    <v-list class="mt-4">
+      <v-list-item
+        v-for="entry in entries"
+        :key="entry.path"
+        :prepend-icon="entry.icon"
+        :title="entry.title"
+        :subtitle="entry.desc"
+        lines="two"
+        :to="$localePath(entry.path)"
+      />
+    </v-list>
+  </div>
 </template>
+
+<script lang="ts" setup>
+usePageTitle(tx("pageTitles.home"))
+
+interface Entry {
+  title: string
+  desc: string
+  path: string
+  icon: string
+}
+
+const pathPrefix = "/genshin"
+
+const entries: Entry[] = [
+  {
+    title: tx("pageTitles.characters"),
+    desc: tx("indexPageGenshin.charactersDesc"),
+    icon: "mdi-account",
+    path: `${pathPrefix}/characters`,
+  },
+  {
+    title: tx("pageTitles.weapons"),
+    desc: tx("indexPageGenshin.weaponsDesc"),
+    icon: "mdi-sword",
+    path: `${pathPrefix}/weapons`,
+  },
+  {
+    title: tx("pageTitles.artifacts"),
+    desc: tx("indexPageGenshin.artifactsDesc"),
+    icon: "mdi-flower",
+    path: `${pathPrefix}/artifacts`,
+  },
+  {
+    title: tx("pageTitles.furnishingSets"),
+    desc: tx("indexPageGenshin.furnishingSetsDesc"),
+    icon: "mdi-sofa-single",
+    path: `${pathPrefix}/furnishing-sets`,
+  },
+  {
+    title: tx("pageTitles.materials"),
+    desc: tx("indexPageGenshin.materialsDesc"),
+    icon: "mdi-grass",
+    path: `${pathPrefix}/materials`,
+  },
+  {
+    title: tx("pageTitles.resinCalc"),
+    desc: tx("indexPageGenshin.resinCalcDesc"),
+    icon: "mdi-moon-waxing-crescent",
+    path: `${pathPrefix}/resin-calc`,
+  },
+  {
+    title: tx("pageTitles.wishes"),
+    desc: tx("indexPageGenshin.wishesDesc"),
+    icon: "mdi-history",
+    path: `${pathPrefix}/wishes`,
+  },
+]
+</script>
--- packages/nuxt/pages/hsr/index.vue
+++ packages/nuxt/pages/hsr/index.vue
@@ -1,14 +1,17 @@
 <template>
-  <div>
+  <div class="d-flex flex-column ga-4">
     <h1 style="font-size: 1.5em">
-      {{ $t("common.appName") }}
+      {{ $t("indexPageHsr.title") }}
     </h1>
+    <p>{{ tx("indexPageHsr.desc") }}</p>
     <v-list class="mt-4">
       <v-list-item
         v-for="entry in entries"
         :key="entry.path"
         :prepend-icon="entry.icon"
         :title="entry.title"
+        :subtitle="entry.desc"
+        lines="two"
         :to="$localePath(entry.path)"
       />
     </v-list>
@@ -20,6 +23,7 @@ usePageTitle(tx("pageTitles.home"))
 
 interface Entry {
   title: string
+  desc: string
   path: string
   icon: string
 }
@@ -29,31 +33,37 @@ const pathPrefix = "/hsr"
 const entries: Entry[] = [
   {
     title: tx("pageTitles.characters"),
+    desc: tx("indexPageHsr.charactersDesc"),
     icon: "mdi-account",
     path: `${pathPrefix}/characters`,
   },
   {
     title: tx("pageTitles.lightCones"),
+    desc: tx("indexPageHsr.lightConesDesc"),
     icon: "mdi-cone",
     path: `${pathPrefix}/light-cones`,
   },
   {
     title: tx("pageTitles.relics"),
+    desc: tx("indexPageHsr.relicsDesc"),
     icon: "mdi-star-david",
     path: `${pathPrefix}/relics`,
   },
   {
     title: tx("pageTitles.materials"),
+    desc: tx("indexPageHsr.materialsDesc"),
     icon: "mdi-grass",
     path: `${pathPrefix}/materials`,
   },
   {
     title: tx("pageTitles.tpCalc"),
+    desc: tx("indexPageHsr.tpCalcDesc"),
     icon: "mdi-sphere",
     path: `${pathPrefix}/tp-calc`,
   },
   {
     title: tx("pageTitles.warps"),
+    desc: tx("indexPageHsr.warpsDesc"),
     icon: "mdi-history",
     path: `${pathPrefix}/warps`,
   },
