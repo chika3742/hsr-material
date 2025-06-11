c0d6f256f0ce92a57696e0103f5021127edeab0a
chika3742 <kazu.chika.shima@gmail.com>
2025-06-11 22:26:16 +0900

    Add Genshin materials data

--- packages/nuxt/types/data/src/materials.ts
+++ packages/nuxt/types/data/src/materials.ts
@@ -1,13 +1,28 @@
 import type { LocalizedText } from "../locales"
 
-export interface Material extends Record<string, unknown> {
+export interface Material {
   id: string
   name: LocalizedText
   yomi: string
+  imageUrl: string
   rarity: number
   category: string
   groupId?: string
   craftLevel?: number
+  source?: MaterialSource
+}
+
+export type MaterialSource = TeyvatMapSource | TextSource
+
+export interface TeyvatMapSource {
+  runtimeType: "teyvatMap"
+  typeId: string
+  center: string
+}
+
+export interface TextSource {
+  runtimeType: "text"
+  text: LocalizedText
 }
 
 export type Materials = Material[]
